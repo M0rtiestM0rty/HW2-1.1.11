9131fe1fd8d35ec6a83f3a1eb4860ed2a633f013
Daisy Arroyo <daisyarroyo@Mac.lan>
2025-02-16 15:50:25 -0500

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+/*Write a code fragment that prints the contents of a two-dimensional boolean array,
+using * to represent true and a space to represent false. Include row and column numbers.*/
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean[][] array =
+        { { true, false,true },
+        { false, true, false },
+        {true,false,true}};
+
+        for(int i = 0; i < array.length; i++)
+        {
+            for(int j = 0; j < array[i].length; j++)
+            {
+                System.out.print(array[i][j] ? "*" : " ");
+            }
+            System.out.println();
         }
-    }
-}
+
+    }//end main
+}//end class
